df8bd35b6a6f82f07817e578ee11fcef3cc68b58
francescoopoggi <95907773+francescoopoggio@users.noreply.github.com>
2022-01-21 11:25:52 +0000

    quarto

--- src/prova4.py
+++ src/prova4.py
@@ -8,12 +8,18 @@ a_numpy = np.array(a)
 
 a = range(0,15) 
 b = a.reshape(3,5) 
+print (b)
 a = np.ones((3,4))
+print (a)
 
 y = np.array([[1,3],[5,6]])
 x = np.copy(y)
+print (y)
+print (x)
 x = np.array([[1,2],[3,4]])
 y = np.swapaxes(x,0,1)
+print (y)
+print (x)
 
 newarr = ar.array('d', [2.1, 4.5, 5.5])
 print(newarr)
@@ -24,7 +30,8 @@ dispari = arr.array('i', [1, 3, 5])
 pari = arr.array('i', [2, 4, 6])
 
 num = arr.array('i')   
-numb = pari + dispari
+num = pari + dispari
+print (num)
 
 num.remove(6)
 print(num) 
@@ -49,5 +56,17 @@ b = np.arange(100)
 dot_product = np.dot(a, b)
 outer_product = np.outer(a, b)
 vector = np.multiply(a, b)
+print (dot_product)
+print (outer_product)
+print (vector)
 
+A = np.array([[3, 6, 7], [5, -3, 0]])
+B = np.array([[1, 1], [2, 1], [3, -3]])
+C = A.dot(B)
+print(C)
+
+
+t = np.arange(0.0, 2.0, 0.1)
+s = 1 + np.sin(2*np.pi*t)np.exp(-t)
+plt.plot(t,s)
 
